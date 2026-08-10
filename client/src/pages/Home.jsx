3e375160e10fa9e6e77e5eebf17942b9458142ba
SCIPIO666 <eaphoney@gmail.com>
2026-08-10 17:53:03 +0300

    feat: staging first deployment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import Hero from './pageComponents/Hero'
 export default function Home() {
   return (
-    <Hero/>
+    <Hero heading='Providing Hope and Healing' tagline='he Nashville Children’s Alliance supports kids who have been abused or exposed to violence. '/>
   )
 }
--- client/src/pages/pageComponents/Hero.jsx
+++ client/src/pages/pageComponents/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { pattern3 } from './SvgPatterns'
+import { pattern3 } from '../../data/SvgPatterns' 
 import CallToActionButton from '../../components/CallToActionButton'
-function Hero({imageUrl,heading='HEADING'}) {
+function Hero({imageUrl,heading='HEADING',tagline=''}) {
   return (
     <div className='min-h-screen bg-[#04211E] relative overflow-hidden'>
 
@@ -9,9 +9,13 @@ function Hero({imageUrl,heading='HEADING'}) {
             className="absolute inset-0 w-full h-full"
             dangerouslySetInnerHTML={{ __html: pattern3 }}
         />        
-        <div className="relative z-10">
-            <h2 className='hero-gradient-text'>{heading}</h2>
-            <CallToActionButton variant='soft' />
+        <div className="relative z-10 max-w-[85%] m-auto">
+            <h2 className='hero-gradient-text align-text-left'>{heading}</h2>
+            <div className='m-auto w-[50%] flex align-middle justify-between gap-8 flex-col'>
+                <p className='font-bold'>{tagline}</p>
+                <CallToActionButton variant='soft' />
+            </div>
+
         </div>
 
         </div>
