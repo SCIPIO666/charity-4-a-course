0b79aff3a53880e43b73dd707cb7f2f50baf901d
SCIPIO666 <eaphoney@gmail.com>
2026-08-11 16:06:42 +0300

    feat: heroe marque aniation

--- client/src/pages/pageComponents/HeroCarousel.jsx
+++ client/src/pages/pageComponents/HeroCarousel.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
 
+import loop1 from '../../../public/loop1.jpg'
+import loop2 from '../../../public/loop2.jpg'
+import loop3 from '../../../public/loop3.jpg'
+import loop4 from '../../../public/loop4.jpg'
+
+const IMAGES = [
+  loop1,
+  loop2,
+  loop3,
+  loop4,
+]
+
 export default function HeroCarousel() {
   return (
-    <div>HeroCarousel</div>
+    <div className="w-full overflow-hidden">
+      
+      <div className="flex w-max animate-scroll gap-4">
+        
+        {[...IMAGES, ...IMAGES].map((image, index) => (
+          <div
+            key={index}
+            className="
+              h-28
+              w-40
+              shrink-0
+              overflow-hidden
+              rounded-xl
+            "
+          >
+            <img
+              src={image}
+              alt=""
+              className="h-full w-full object-cover"
+            />
+          </div>
+        ))}
+
+      </div>
+
+    </div>
   )
 }
