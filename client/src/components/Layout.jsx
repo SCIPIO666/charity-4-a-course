d929e66887c4aa90533bbb37d75e531eb6bf9aff
SCIPIO666 <eaphoney@gmail.com>
2026-08-18 18:51:01 +0300

    fix: import errors

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import OrganizationSchema from '../pages/pageComponents/OrganizationSchema'
+import OrganizationSchema from '../pages/pageComponents/OrganizationSchema' //error in prod
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
